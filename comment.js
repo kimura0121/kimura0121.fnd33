6da312dd2a7af84bbd5d0ca0aaeb107d7d8aaf51
kimura0121 <yuuki_kimura_aa@mail.toyota.co.jp>
2025-01-16 16:43:27 +0900

    Update comment.js

--- comment.js
+++ comment.js
@@ -1,21 +1,11 @@
 function comment(content){
-
-    // 空のコメントを宣言
-    let comment = "";
-    // imgArea取得
-    let imgArea = document.getElementById("imgArea");
-    // 乱数取得
-    let random = Math.floor( Math.random() * document.querySelector("#imgArea").childElementCount );
-    // ランダムなidを取得
-    let name = imgArea.children[random].id;
-
     switch(content) {
         case "admission":
-            comment = "第一回シスカサマーダッシュ元町競馬場は晴れ,馬場状態は良,本日の一番人気" + name + "。今年はどんな花を咲かせるのか!!"
+            comment = "第一回シスカサマーダッシュ元町競馬場は晴れ,馬場状態は良"
         case "start":
-            comment = "第一回シスカサマーダッシュ今スタートが切られました、" + name + "がいいスタートを決めてくれました"
+            comment = "第一回シスカサマーダッシュ今スタートが切られました、"
         case "last":
-            comment = name + "が追い上げる!最後100m追いつけるのか!!"
+            comment ="最後100m追いつけるのか!!"
     }
 
     // commentAreaを取得
